a3d2c37b3eb6039528c4c1ca05b428e2f32849af
Bharat <hibharatj@gmail.com>
2022-04-30 20:12:44 +0530

    Perfection in login and sign up page (Final Completion)

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy # Setting Up our database
 from os import path
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()   # Defining the database
@@ -14,6 +15,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # It will make a database folder to store all the data
     db.init_app(app)
     
+
+    
     # Registering our blueprints in init.py
     from .views import views
     from .auth import auth
@@ -25,6 +28,13 @@ def create_app():
         
     create_database(app)
 
+    login_manager = LoginManager() # imported login manager
+    login_manager.login_view = 'auth.login' # page to show if the user is not logged in 
+    login_manager.init_app(app)
+
+    @login_manager.user_loader  # Telling flask how we load a user
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
@@ -10,11 +12,31 @@ auth = Blueprint('auth', __name__)
 def login():
     # data = request.form
     # print(data)   # In this manner we can request data from the form and print it to the console
-    return render_template('login.html')
+    # ABCD
+    if request.method =='POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()    # Checks if there is any user in the database with entered email 
+        if user:
+            if check_password_hash(user.password, password):
+                # if the password entered on login end and their saved password match, then it'll login
+                flash('Logged In Successfully!', category='success')
+                login_user(user,remember=True) # This remembers that the user has logged in until it logs out # import from flask_login
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Password', category='error')
+        else:
+            flash('Email Does Not exist.', category='error')
+        
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "Logout"
+    logout_user()
+    flash('See You Soon')
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -27,8 +49,11 @@ def sign_up():
         password2 = request.form.get('password2')
         # print(email, firstName, password1, password2)
         
+        user = User.query.filter_by(email=email).first()    # To check if entered email already exists
+        if user:
+            flash('Email already exists.', category='error')
         # Checking if the email is correct, firstName is valid and both the passwords match and are greater than 7 digits
-        if len(email) < 4:
+        elif len(email) < 4:
             flash("Email must be greater than 4 characters", category='error')
             # print('Email error')
         elif len(first_name) < 2:
@@ -41,12 +66,14 @@ def sign_up():
             flash("Password must be greater than 7 characters", category='error')
             # print('password length error')
         else:
-            flash('Account created Successfully', category='success')
             # add user to database
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True) # If user signed in successfully, log in that user
+            flash('Account created!', category='success')
             return redirect(url_for('views.home'))
+        
             
 
-    return render_template('sign-up.html')
+    return render_template('sign-up.html', user=current_user)
